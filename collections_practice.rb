bea8531e21f7612ff6ce751e43868d3885b38ecd
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-25 17:40:42 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,38 @@
+def sort_array_asc(arr)
+  arr.sort
+end
 
+def sort_array_desc(arr)
+  arr.sort.reverse
+end
+
+def sort_array_char_count(arr)
+  arr.sort{|a,b| a.length<=>b.length}
+end
+
+def swap_elements(arr)
+  arr[1],arr[2] = arr[2],arr[1]
+  arr
+end
+
+def reverse_array(arr)
+  arr.reverse
+end
+
+def kesha_maker(arr)
+  new_arr = []
+  arr.each{|str| str[2] = "$"; new_arr << str}
+  new_arr
+end
+
+def find_a(arr)
+  arr.select{|str| str.start_with? "a"}
+end
+
+def sum_array(arr)
+  arr.inject{|sum,n| sum + n}
+end
+
+def add_s(arr)
+  arr.map.with_index{|n,i| i!=1 ? n+"s" : n}
+end
